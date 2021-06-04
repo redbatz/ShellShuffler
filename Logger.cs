620e8ea4c22c6849ea4484189e95334b72f0f5a5
ajkroeg <50463892+ajkroeg@users.noreply.github.com>
2021-06-04 19:53:56 -0400

    Update Logger.cs

--- Logger.cs
+++ Logger.cs
@@ -26,7 +26,7 @@ namespace ShellShuffler
         {
             if (enableLogging)
             {
-                string ts = DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+                string ts = DateTime.Now.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
                 logStreamWriter.WriteLine($"INFO: {ts} - {message}");
             }
         }
@@ -34,13 +34,13 @@ namespace ShellShuffler
 
         public void LogError(string message)
         {
-            string ts = DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+            string ts = DateTime.Now.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
             logStreamWriter.WriteLine($"ERROR: {ts} - {message}");
         }
 
         public void LogException(Exception exception)
         {
-            string ts = DateTime.UtcNow.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
+            string ts = DateTime.Now.ToString("s", System.Globalization.CultureInfo.InvariantCulture);
             logStreamWriter.WriteLine($"CRITICAL: {ts} - {exception}");
         }
     }
